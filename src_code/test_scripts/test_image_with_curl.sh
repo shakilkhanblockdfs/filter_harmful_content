cbab22f8906ce985e4895e2aeea5aa737f2b81ab
shakil <shakilk1729@gmail.com>
2026-02-17 03:11:16 -0800

    Working but the image model is not able to detect nude pics properly. Need to fix the bug list in bug.txt

--- src_code/test_scripts/test_image_with_curl.sh
+++ src_code/test_scripts/test_image_with_curl.sh
@@ -1,2 +1,3 @@
 # curl -X POST "http://127.0.0.1:8000/moderate_image" -F "file=@./hate.png"
-curl -X POST "http://127.0.0.1:8000/moderate_image" -F "file=@./riot.png"
+#curl -X POST "http://127.0.0.1:8000/moderate_image" -F "file=@./riot.png"
+curl -X POST "http://127.0.0.1:8000/moderate_image" -F "file=@./bits.jpg"
--- src_code/test_scripts/test_text_with_curl.sh
+++ src_code/test_scripts/test_text_with_curl.sh
@@ -1,7 +1,7 @@
 curl -X POST "http://127.0.0.1:8000/moderate" \
 -H "Content-Type: application/json" \
 -d '{
-"text": "You are stupid and I hate you",
+"text": "You are intelligent and smart",
 "user_id": "user42"
 }'
 
